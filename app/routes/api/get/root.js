20630b9de0d8b408b0ade4bdffed30dc0748f81d
Ross Hudson <hi@honeypie.io>
2018-09-22 13:00:00 +0100

    Fixed "last login" feature for users.

--- app/routes/api/get/root.js
+++ app/routes/api/get/root.js
@@ -4,6 +4,7 @@ var router = require("express").Router();
 
 router.use("/members", require("./members/root"))
 router.use("/reports", require("./reports/root"))
+router.use("/users", require("./users/root"))
 router.use("/working-groups", require("./working-groups/root"))
 
 module.exports = router;
