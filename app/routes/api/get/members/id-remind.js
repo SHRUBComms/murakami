99926700c9577079d9a9c20a64811db521d25f75
Ross Hudson <hello@rosshudson.co.uk>
2019-01-26 16:31:19 +0000

    Readded volunteer id reminder button.

--- app/routes/api/get/members/id-remind.js
+++ app/routes/api/get/members/id-remind.js
@@ -9,35 +9,37 @@ var Members = require(rootDir + "/app/models/members");
 var Auth = require(rootDir + "/app/configs/auth");
 var Mail = require(rootDir + "/app/configs/mail");
 
-router.get("/:member_id", Auth.isLoggedIn, Auth.isOfClass(["admin"]), function(
-  req,
-  res
-) {
-  Members.getById(req.params.member_id, { class: "admin" }, function(
-    err,
-    member
-  ) {
-    if (!member || err) {
-      req.flash("error", "Member not found");
-      res.redirect("/members");
-    } else {
-      Mail.sendAutomated("membership_id_reminder", member.member_id, function(
-        err
-      ) {
-        if (err) {
-          req.flash("error", "Something went wrong!");
-          res.redirect("/members");
-        } else {
-          req.flash("success_msg", "Member has been sent their ID");
-          res.redirect(
-            process.env.PUBLIC_ADDRESS +
-              "/volunteers/view/" +
-              req.params.member_id
-          );
-        }
-      });
-    }
-  });
-});
+router.get(
+  "/:member_id",
+  Auth.isLoggedIn,
+  Auth.isOfClass(["admin", "staff"]),
+  function(req, res) {
+    Members.getById(req.params.member_id, { class: "admin" }, function(
+      err,
+      member
+    ) {
+      if (!member || err) {
+        req.flash("error", "Member not found");
+        res.redirect("/members");
+      } else {
+        Mail.sendAutomated("membership_id_reminder", member.member_id, function(
+          err
+        ) {
+          if (err) {
+            req.flash("error", "Something went wrong!");
+            res.redirect("/members");
+          } else {
+            req.flash("success_msg", "Volunteer has been sent their ID");
+            res.redirect(
+              process.env.PUBLIC_ADDRESS +
+                "/volunteers/view/" +
+                req.params.member_id
+            );
+          }
+        });
+      }
+    });
+  }
+);
 
 module.exports = router;
