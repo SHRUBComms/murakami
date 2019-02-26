232d303594ec171ca27cce8ab5a0aaba92734f91
Ross Hudson <hello@rosshudson.co.uk>
2019-02-26 13:06:39 +0000

    Publicly accessible volunteer role summary Note: will redirect to shrubcoop.org in future.

--- app/routes/volunteers/roles/view.js
+++ app/routes/volunteers/roles/view.js
@@ -10,23 +10,21 @@ var Volunteers = require(rootDir + "/app/models/volunteers");
 var Auth = require(rootDir + "/app/configs/auth");
 var Helpers = require(rootDir + "/app/configs/helpful_functions");
 
-router.get(
-  "/:role_id",
-  Auth.isLoggedIn,
-  Auth.isOfClass(["admin", "staff"]),
-  function(req, res) {
-    Volunteers.getRoleById(req.params.role_id, function(err, role) {
+router.get("/:role_id", function(req, res) {
+  Volunteers.getRoleById(req.params.role_id, function(err, role) {
+    WorkingGroups.getById(role.group_id, function(err, group) {
       if (role) {
         res.render("volunteers/roles/view", {
           title: "View Volunter Role",
           volunteerRolesActive: true,
-          role: role
+          role: role,
+          group: group[0]
         });
       } else {
         res.redirect(process.env.PUBLIC_ADDRESS + "/volunteers/roles/manage");
       }
     });
-  }
-);
+  });
+});
 
 module.exports = router;
