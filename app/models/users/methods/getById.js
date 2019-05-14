89e4720a97f8f8d227694e8d4d5d39fd60cfae1c
Ross Hudson <hello@rosshudson.co.uk>
2019-05-14 14:55:42 +0100

    Fixed login bug.

--- app/models/users/methods/getById.js
+++ app/models/users/methods/getById.js
@@ -4,19 +4,19 @@ module.exports = function(Users, sequelize, DataTypes) {
                 LEFT JOIN (SELECT user_id login_user_id, MAX(login_timestamp) lastLogin
                 FROM attempts GROUP BY user_id) attempts ON users.id=attempts.login_user_id
                 LEFT JOIN data_permissions ON data_permissions.class=users.class
-                WHERE users.id = id`;
+                WHERE users.id = ?`;
 
     sequelize
       .query(query, {
-        replacements: {
-          id: id,
-          type: DataTypes.QueryTypes.SELECT
-        }
+        replacements: [id],
+        type: DataTypes.QueryTypes.SELECT
       })
       .nodeify(function(err, user) {
         try {
-          if (user[0][0]) {
-            Users.sanitizeUser(user[0], loggedInUser, function(sanitizedUser) {
+          console.log(err, user);
+          if (user[0]) {
+            Users.sanitizeUser(user, loggedInUser, function(sanitizedUser) {
+              console.log(err, sanitizedUser[0]);
               callback(err, sanitizedUser[0]);
             });
           } else {
--- app/models/users/methods/sanitizeUser.js
+++ app/models/users/methods/sanitizeUser.js
@@ -7,136 +7,142 @@ var Helpers = require(process.env.CWD + "/app/helper-functions/root");
 
 module.exports = function(Users, sequelize, DataTypes) {
   return function(users, loggedInUserObj, callback) {
+    console.log("Users:", users);
     var usersObj = {};
+    var sanitizedUsers = [];
 
     async.eachOf(
       users,
       function(user, i, loopCallback) {
-        var sanitizedUser = {};
-        var loggedInUser = lodash.clone(loggedInUserObj);
-        var isUser = false;
+        if (user) {
+          console.log("user found");
+          var sanitizedUser = {};
 
-        if (user.id == loggedInUser.id) {
-          isUser = true;
-          loggedInUser.permissions = {
-            users: {
-              name: true,
-              email: true,
-              username: true,
-              working_groups: true
+          var loggedInUser = lodash.clone(loggedInUserObj);
+          var isUser = false;
+
+          if (user.id == loggedInUser.id) {
+            isUser = true;
+            loggedInUser.permissions = {
+              users: {
+                name: true,
+                email: true,
+                username: true,
+                working_groups: true
+              }
+            };
+          }
+
+          if (user.working_groups) {
+            user.working_groups = user.working_groups || [];
+          }
+
+          user.full_name = user.first_name + " " + user.last_name;
+
+          if (user.notification_preferences && isUser) {
+            user.notification_preferences = user.notification_preferences || {};
+          } else {
+            user.notification_preferences = {};
+          }
+
+          if (!user.lastLogin) {
+            user.lastLogin = "Never";
+          }
+
+          if (!isUser) {
+            delete user.password;
+          }
+
+          var commonWorkingGroup = Helpers.hasOneInCommon(
+            loggedInUser.working_groups,
+            user.working_groups
+          );
+
+          try {
+            if (
+              loggedInUser.permissions.users.name == true ||
+              (loggedInUser.permissions.users.name == "commonWorkingGroup" &&
+                commonWorkingGroup) ||
+              isUser
+            ) {
+              sanitizedUser.first_name = user.first_name;
+              sanitizedUser.last_name = user.last_name;
+              sanitizedUser.full_name = user.full_name;
+              sanitizedUser.name = user.full_name;
             }
-          };
-        }
+          } catch (err) {}
 
-        if (user.working_groups) {
-          user.working_groups = user.working_groups || [];
-        }
+          try {
+            if (
+              loggedInUser.permissions.users.email == true ||
+              (loggedInUser.permissions.users.email == "commonWorkingGroup" &&
+                commonWorkingGroup) ||
+              isUser
+            ) {
+              sanitizedUser.email = user.email;
+            }
+          } catch (err) {}
 
-        user.full_name = user.first_name + " " + user.last_name;
+          try {
+            if (
+              loggedInUser.permissions.users.username == true ||
+              (loggedInUser.permissions.users.username ==
+                "commonWorkingGroup" &&
+                commonWorkingGroup) ||
+              isUser
+            ) {
+              sanitizedUser.username = user.username;
+            }
+          } catch (err) {}
 
-        if (user.notification_preferences && isUser) {
-          user.notification_preferences = user.notification_preferences || {};
+          try {
+            if (
+              loggedInUser.permissions.users.workingGroups == true ||
+              (loggedInUser.permissions.users.workingGroups ==
+                "commonWorkingGroup" &&
+                commonWorkingGroup) ||
+              isUser
+            ) {
+              sanitizedUser.working_groups = user.working_groups;
+            }
+          } catch (err) {}
+
+          try {
+            if (
+              loggedInUser.permissions.users.update == true ||
+              (loggedInUser.permissions.users.update == "commonWorkingGroup" &&
+                commonWorkingGroup) ||
+              isUser
+            ) {
+              sanitizedUser.canUpdate = true;
+            }
+          } catch (err) {}
+
+          if (Object.keys(sanitizedUser).length > 0) {
+            sanitizedUser.notification_preferences =
+              user.notification_preferences || {};
+            sanitizedUser.lastLogin = user.lastLogin || "Never";
+            sanitizedUser.password = user.password || null;
+            sanitizedUser.id = user.id;
+            sanitizedUser.class = user.class;
+            sanitizedUser.deactivated = user.deactivated;
+
+            if (user.permissions) {
+              sanitizedUser.permissions = user.permissions;
+            }
+
+            usersObj[user.id] = sanitizedUser;
+            sanitizedUsers.push(sanitizedUser);
+            loopCallback();
+          } else {
+            loopCallback();
+          }
         } else {
-          user.notification_preferences = {};
-        }
-
-        if (!user.lastLogin) {
-          user.lastLogin = "Never";
-        }
-
-        if (!isUser) {
-          delete user.password;
-        }
-
-        var commonWorkingGroup = Helpers.hasOneInCommon(
-          loggedInUser.working_groups,
-          user.working_groups
-        );
-
-        try {
-          if (
-            loggedInUser.permissions.users.name == true ||
-            (loggedInUser.permissions.users.name == "commonWorkingGroup" &&
-              commonWorkingGroup) ||
-            isUser
-          ) {
-            sanitizedUser.first_name = user.first_name;
-            sanitizedUser.last_name = user.last_name;
-            sanitizedUser.full_name = user.full_name;
-            sanitizedUser.name = user.full_name;
-          }
-        } catch (err) {}
-
-        try {
-          if (
-            loggedInUser.permissions.users.email == true ||
-            (loggedInUser.permissions.users.email == "commonWorkingGroup" &&
-              commonWorkingGroup) ||
-            isUser
-          ) {
-            sanitizedUser.email = user.email;
-          }
-        } catch (err) {}
-
-        try {
-          if (
-            loggedInUser.permissions.users.username == true ||
-            (loggedInUser.permissions.users.username == "commonWorkingGroup" &&
-              commonWorkingGroup) ||
-            isUser
-          ) {
-            sanitizedUser.username = user.username;
-          }
-        } catch (err) {}
-
-        try {
-          if (
-            loggedInUser.permissions.users.workingGroups == true ||
-            (loggedInUser.permissions.users.workingGroups ==
-              "commonWorkingGroup" &&
-              commonWorkingGroup) ||
-            isUser
-          ) {
-            sanitizedUser.working_groups = user.working_groups;
-          }
-        } catch (err) {}
-
-        try {
-          if (
-            loggedInUser.permissions.users.update == true ||
-            (loggedInUser.permissions.users.update == "commonWorkingGroup" &&
-              commonWorkingGroup) ||
-            isUser
-          ) {
-            sanitizedUser.canUpdate = true;
-          }
-        } catch (err) {}
-
-        if (Object.keys(sanitizedUser).length > 0) {
-          sanitizedUser.notification_preferences =
-            user.notification_preferences || {};
-          sanitizedUser.lastLogin = user.lastLogin || "Never";
-          sanitizedUser.password = user.password || null;
-          sanitizedUser.id = user.id;
-          sanitizedUser.class = user.class;
-          sanitizedUser.deactivated = user.deactivated;
-
-          if (user.permissions) {
-            sanitizedUser.permissions = user.permissions;
-          }
-
-          usersObj[user.id] = sanitizedUser;
-          users[i] = sanitizedUser;
-          loopCallback();
-        } else {
-          users[i] = {};
-          usersObj[user.id] = {};
-
           loopCallback();
         }
       },
       function() {
-        callback(users, usersObj);
+        callback(sanitizedUsers, usersObj);
       }
     );
   };
