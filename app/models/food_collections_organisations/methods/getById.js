1055a3ed77f0b47d0b0b02119d1775d4b5ec6c5f
Ross Hudson <hello@rosshudson.co.uk>
2020-06-18 10:07:07 +0100

    Small bug fix.

--- app/models/food_collections_organisations/methods/getById.js
+++ app/models/food_collections_organisations/methods/getById.js
@@ -1,11 +1,12 @@
 module.exports = function(FoodCollectionsOrganisations, sequelize, DataTypes) {
   return function(organisation_id, callback) {
     var query =
-      "SELECT * FROM food_collections_organisations LEFT JOIN (SELECT organisation_id collections_organisation_id, MAX(timestamp) lastCollection FROM food_collections GROUP BY organisation_id) collections ON food_collections_organisations.organisation_id=collections.collections_organisation_id WHERE food_collections_organisations.organisation_id = ?";
+      "SELECT * FROM food_collections_organisations LEFT JOIN (SELECT collection_organisation_id AS organisation_id, MAX(timestamp) AS lastCollection FROM food_collections GROUP BY organisation_id) collections ON food_collections_organisations.organisation_id=collections.organisation_id WHERE food_collections_organisations.organisation_id = ?";
 
     sequelize
       .query(query, { replacements: [organisation_id] })
       .nodeify(function(err, organisation) {
+        console.log(err, organisation);
         if (organisation[0][0]) {
           callback(err, organisation[0][0]);
         } else {
