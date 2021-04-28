888cbd70ada4c3b5cdda474a7fed7ac6a20399a0
Ross Hudson <hello@rosshudson.co.uk>
2021-04-28 14:58:38 +0100

    Allow 0% discounts for reporting purposes

--- app/controllers/tills/categories/validateDiscount.js
+++ app/controllers/tills/categories/validateDiscount.js
@@ -5,7 +5,7 @@ const Validators = require(rootDir + "/app/controllers/validators");
 
 const sanitizeCategory = async (category, stockCategories) => {
   await Validators.string({ name: "discount name", indefiniteArticle: "a", value: category.name }, { required: true, minLength: 0, maxLength: 31});
-  await Validators.number({ name: "discount amount", indefiniteArticle: "a", value: category.value }, { required: true, min: 1, max: 100 });
+  await Validators.number({ name: "discount amount", indefiniteArticle: "a", value: category.value }, { required: true, min: 0, max: 100 });
 
   if (category.parent && !stockCategories[category.parent]) {
     throw "Select a valid parent or leave blank";
