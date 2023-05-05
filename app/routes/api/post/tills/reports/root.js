44444dd904b43c67c0fb4a0c520603fc35e2097f
Ross Hudson <hello@rosshudson.co.uk>
2023-05-05 17:31:47 +0100

    Added route to get till transactions via API

--- app/routes/api/post/tills/reports/root.js
+++ app/routes/api/post/tills/reports/root.js
@@ -4,6 +4,7 @@ const router = require("express").Router();
 
 router.use("/quick-summary", require("./quick-summary"));
 router.use("/transactions", require("./transactions"));
+router.use("/transactions-by-project", require("./transactions-by-project"));
 router.use("/floats", require("./floats"));
 router.use("/revenue", require("./revenue"));
 router.use("/unit-sales", require("./unit-sales"));
---
+++ app/routes/api/post/tills/reports/transactions-by-project.js
@@ -0,0 +1,60 @@
+// /api/post/tills/reports/transactions-by-project
+
+const router = require("express").Router();
+
+const moment = require("moment");
+moment.locale("en-gb");
+
+const rootDir = process.env.CWD;
+
+const Models = require(rootDir + "/app/models/sequelize");
+const Tills = Models.Tills;
+const Transactions = Models.Transactions;
+const Members = Models.Members;
+const StockCategories = Models.StockCategories;
+
+const Auth = require(rootDir + "/app/controllers/auth");
+const Helpers = require(rootDir + "/app/controllers/helper-functions/root");
+
+router.post("/", Auth.verifyByKey("tillRevenue"), async (req, res) => {  
+  try {
+    const till_id = req.body.till_id;
+    const datePeriod = req.body.datePeriod || "today";
+
+    const startDateRaw = req.body.startDate || null;
+    const endDateRaw = req.body.endDate || null;
+
+    if (!till_id) {
+      throw "No till specified";
+    }
+
+    const till = await Tills.getById(till_id);
+
+    if (!till) {
+      throw "Till not found";
+    }
+    
+    const { formattedStartDate, formattedEndDate } = await Helpers.plainEnglishDateRangeToDates(datePeriod, startDateRaw, endDateRaw);
+    const transactions = await Transactions.getAllBetweenTwoDatesByTillId(till_id, formattedStartDate, formattedEndDate);
+    
+    const { membersObj } = await Members.getAll();
+    const categories = await StockCategories.getCategories("treeKv");
+    const formattedTransactions = await Transactions.formatTransactions(transactions, membersObj, categories, till_id);
+
+    let sanitizedFormattedTransactions = [];
+
+    for await (const transaction of formattedTransactions) {
+      if (!transaction.paymentFailed && !transaction.isRefund) {
+        // Add month key to each transaction to make it easier to sort data in spreadsheet frontend.
+        transaction.monthKey = moment(transaction.date).startOf("month").format("YYYY-MM-DD");
+        sanitizedFormattedTransactions.push(transaction);
+      }
+    }
+
+    res.send(sanitizedFormattedTransactions);
+  } catch (error) {
+    console.log(error);
+    res.send([]);
+  }
+});
+module.exports = router;
