d33665fa325d592f1f4011b3b30247ccfc6c3407
pkeyss <peterbkeys@gmail.com>
2024-05-21 19:30:21 +0100

    Add OAuth2 for sumup

--- app/models/settings/methods/updateSetting.js
+++ app/models/settings/methods/updateSetting.js
@@ -1,5 +1,15 @@
 module.exports = (Settings, sequelize, DataTypes) => {
+  /**
+   * Creates a new setting if it doesn't already exist.
+   * @param {string} id - The ID of the setting.
+   * @param {Object} data - The data to be saved in the setting.
+   * @returns {Promise<Object>} A promise that resolves to the setting instance.
+   */
   return async (id, data) => {
-    return Settings.update({ data: data }, { where: { id: id } });
+    const [setting, created] = await Settings.findOrCreate({
+      where: { id: id },
+      defaults: { id: id, data: data }
+    });
+    return setting;
   }
 }
