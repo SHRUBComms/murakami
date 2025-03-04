d60d0e42c4b627abe82e080018d6297e72e9da3c
JBSmith-Git <jbsmith.oakfield@outlook.com>
2025-03-04 20:24:47 +0000

    Change stock categories schema to have nullable vat category

--- app/models/stock_categories/schema.js
+++ app/models/stock_categories/schema.js
@@ -80,7 +80,7 @@ module.exports = function (sequelize, DataTypes) {
       },
       vat_category: {
         type: DataTypes.STRING(100),
-        allowNull: false,
+        allowNull: true,
       }
     },
     {
