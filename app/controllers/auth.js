18af040a5ea9e43bc47ca1dcc72feb8014fdfa17
JBSmith-Git <jbsmith.oakfield@outlook.com>
2025-03-11 22:56:00 +0000

    Add transactions by weekday by hour report

--- app/controllers/auth.js
+++ app/controllers/auth.js
@@ -56,6 +56,7 @@ Auth.verifyByKey = (resource) => {
       "carbonAccountingReport",
       "publicVolunteerRoles",
       "tillRevenue",
+      "transactionsByWeekdayByHourReport",
       "membershipReport",
       "membershipSignUp",
     ];
