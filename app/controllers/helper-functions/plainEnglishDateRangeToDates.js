7eb0b8d1b7349788e113ae075886f8d1e6fbcb3c
JBSmith-Git <jbsmith.oakfield@outlook.com>
2025-06-10 19:30:38 +0100

    Add yesterday, last-week, last-month and last-year as options in the till dashboard quick summary

--- app/controllers/helper-functions/plainEnglishDateRangeToDates.js
+++ app/controllers/helper-functions/plainEnglishDateRangeToDates.js
@@ -13,18 +13,42 @@ module.exports = async (period, startDate, endDate) => {
         throw Error;
       }
     } else {
-      formattedEndDate = moment().endOf("day").toDate();
-
-      if (period == "all-time") {
-        formattedStartDate = moment("1970-01-01").toDate();
-      } else if (period == "year") {
-        formattedStartDate = moment().startOf("year").toDate();
+      // Current time periods:
+      // "week", "month", "year", "all-time"
+      if (period == "week") {
+        formattedStartDate = moment().startOf("week").toDate();
+        formattedEndDate = moment().endOf("day").toDate();
       } else if (period == "month") {
         formattedStartDate = moment().startOf("month").toDate();
-      } else if (period == "week") {
-        formattedStartDate = moment().startOf("week").toDate();
-      } else {
+        formattedEndDate = moment().endOf("day").toDate();
+      } else if (period == "year") {
+        formattedStartDate = moment().startOf("year").toDate();
+        formattedEndDate = moment().endOf("day").toDate();
+      } else if (period == "all-time") {
+        formattedStartDate = moment("1970-01-01").toDate();
+        formattedEndDate = moment().endOf("day").toDate();
+      }
+
+      // Previous time periods:
+      // "yesterday", "last-week", "last-month", "last-year"
+      else if (period == "yesterday") {
+        formattedStartDate = moment().subtract(1, "days").startOf("day").toDate();
+        formattedEndDate = moment().subtract(1, "days").endOf("day").toDate();
+      } else if (period == "last-week") {
+        formattedStartDate = moment().subtract(1, "weeks").startOf("week").toDate();
+        formattedEndDate = moment().subtract(1, "weeks").endOf("week").toDate();
+      } else if (period == "last-month") {
+        formattedStartDate = moment().subtract(1, "months").startOf("month").toDate();
+        formattedEndDate = moment().subtract(1, "months").endOf("month").toDate();
+      } else if (period == "last-year") {
+        formattedStartDate = moment().subtract(1, "years").startOf("year").toDate();
+        formattedEndDate = moment().subtract(1, "years").endOf("year").toDate();
+      }
+
+      // Default to "today"
+      else {
         formattedStartDate = moment().startOf("day").toDate();
+        formattedEndDate = moment().endOf("day").toDate();
       }
     }
   } catch (error) {
